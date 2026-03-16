b4652a52c0f97e6a98397b201fde91eaf5debe2f
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-03-16 15:36:44 -0500

    update to main page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
+      <h1 className="text-2xl font-bold mb-4">AgentOne Documentation</h1>
       <p>
-        You can open{" "}
+        Click to view the{" "}
         <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{" "}
-        and see the documentation.
+          documentation
+        </Link>
+        . Don't know how you wound up here?{" "}
+        <Link href="https://agent-one.dev" className="font-medium underline">
+          View the main website
+        </Link>
+        .
       </p>
     </div>
   );
