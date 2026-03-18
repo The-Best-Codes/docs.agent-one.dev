8936baf5756e7c3a2861185477e57a0ab2789610
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-03-18 10:39:28 -0500

    add beta banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
-import "./global.css";
+import { Banner } from "fumadocs-ui/components/banner";
 import { Inter } from "next/font/google";
+import "./global.css";
+import Link from "next/link";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -10,6 +12,16 @@ export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Banner>
+          These docs are under construction. AgentOne is currently available as
+          a private beta.
+          <Link
+            className="text-fd-info ml-1"
+            href="https://go.bestcodes.dev/discord"
+          >
+            Join the Discord
+          </Link>
+        </Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
