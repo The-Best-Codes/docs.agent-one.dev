42a0de203488f2bf3de7461f96b8bcca975c3f2a
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-03-30 15:43:28 -0500

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
-import { Banner } from "fumadocs-ui/components/banner";
 import { Inter } from "next/font/google";
 import "./global.css";
-import Link from "next/link";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -12,16 +10,6 @@ export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <Banner>
-          These docs are under construction. AgentOne is currently available as
-          a private beta.
-          <Link
-            className="text-fd-info ml-1"
-            href="https://go.bestcodes.dev/discord"
-          >
-            Join the Discord
-          </Link>
-        </Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
