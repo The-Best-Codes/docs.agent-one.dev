04cfe826ea63ab1da56285d4e4b275a53fe2fef1
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-04-24 18:06:58 -0500

    tweaks
    Signed-off-by: The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,15 +9,20 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">AgentOne Documentation</h1>
+      <h1 className="text-2xl font-bold mb-4">
+        Welcome to the AgentOne Documentation!
+      </h1>
       <p>
-        Click to view the{" "}
-        <Link href="/docs" className="font-medium underline">
-          documentation
+        If you're looking for the main website, visit{" "}
+        <Link
+          href="https://www.agent-one.dev"
+          className="font-medium underline"
+        >
+          www.agent-one.dev
         </Link>
-        . Don't know how you wound up here?{" "}
-        <Link href="https://agent-one.dev" className="font-medium underline">
-          View the main website
+        . Otherwise,
+        <Link href="/docs" className="font-medium underline">
+          click here to read the main documentation
         </Link>
         .
       </p>
--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -10,7 +10,7 @@ export const gitConfig = {
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "AgentOne",
+      title: "AgentOne Docs",
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
